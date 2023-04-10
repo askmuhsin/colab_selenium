1c2c7e89ea785f68c9baa51638a6f77a5232946c
Muhsin Mohammed <muhsinmohammed@airasia.com>
2023-04-10 18:08:50 +0800

    bug fix

--- utils.py
+++ utils.py
@@ -1,20 +1,19 @@
 from selenium_profiles.driver import driver as mydriver
 from selenium_profiles.profiles import profiles
-from selenium.webdriver.common.by import By  # locate elements
-from selenium_profiles.utils.colab_utils import display, showscreen, show_html # virtual display
+from selenium.webdriver.common.by import By
+from selenium_profiles.utils.colab_utils import display, showscreen, show_html
 
 from webdriver_manager.chrome import ChromeDriverManager
 
 def setup_driver():
     chromedriver_path = ChromeDriverManager(version="90.0.4430.24").install()
 
-    mydriver = mydriver()
-    display = display()
+    md = mydriver()
+    dis = display()
 
-    display.start_display()
+    dis.start_display()
 
     profile = profiles.Windows() # or .Android
     profile["cdp"]["cores"] = None # Chrome 90 doesn't allow emulating cores :(
-    driver = mydriver.start(profile, uc_driver=False, executable_path=chromedriver_path)
+    driver = md.start(profile, uc_driver=False, executable_path=chromedriver_path)
     return driver
-
